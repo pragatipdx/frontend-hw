41918af99a334caf746280f24f4676b15780d6c8
pragatipdx <pragati@pdx.edu>
2021-10-31 10:23:53 -0700

    Palindrome homework

--- hw3/04-palindrome.js
+++ hw3/04-palindrome.js
@@ -1,3 +1,36 @@
-// Enter your code here
+let userInput = document.querySelector('input');
+let final = document.getElementById("result");
 
-console.log('Enter your code here');
+userInput.addEventListener('input', function(evt) {
+    let input_val = userInput.value;
+
+    // Validation of a valid input number
+    if(isNaN(parseFloat(input_val))){
+      final.innerText = "Please enter a number";
+    }
+
+    // Validate string for a Palindrome
+    else{
+     if(fetchPalindrom(input_val))
+     {
+      final.innerText="Yes, This is a palindrome";
+      final.style.color = "green";
+     }
+    else
+    {
+      final.innerText = "No, Try again";
+      final.style.color = "red";
+    }
+    }
+    
+});
+
+function fetchPalindrom(str){
+    var reverse_str = str.split("").reverse().join("");
+    if(str === reverse_str)
+    return true;
+    else return false;
+  }
+  
+
+
